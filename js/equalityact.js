3458ae3c3f314625fcb5a53a7300b075aabf8d60
Joeet33 <up931673@myport.ac.uk>
2020-03-23 11:57:34 +0000

    added text resize

--- js/equalityact.js
+++ js/equalityact.js
@@ -1,11 +1,18 @@
-$("#small_a").click(function(){
-    $("main").removeid().addid("mainsmall")
-});
-
-$("#medium_a").click(function(){
-    $("main").removeClass().addClass("mainmedium")
-});
-
-$("#large_a").click(function(){
-    $("main").removeClass().addClass("mainlarge")
-});
+function changeTSize(nu) {
+  var el = document.getElementsByTagName('body')[0]; 
+     // only one body in document
+  var fontSizeString = window
+     .getComputedStyle(el, null)
+     .getPropertyValue('font-size');
+  var fontSize = parseFloat(fontSizeString); 
+  el.style.fontSize = (fontSize + nu) + 'px';
+    // Must be done separately for buttons:
+  var els = document.getElementsByTagName('button');
+  for (var i = 0; i < els.length; i++) {
+     fontSizeString = window
+        .getComputedStyle(els[i], null)
+        .getPropertyValue('font-size');
+     var fontSize = parseFloat(fontSizeString); 
+     els[i].style.fontSize = (fontSize + nu) + 'px';
+  }
+ }
