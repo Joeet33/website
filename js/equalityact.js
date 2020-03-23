f4287cfb3da0b65929bc3531e323f1a995583d50
Joeet33 <up931673@myport.ac.uk>
2020-03-23 12:24:22 +0000

    buttons don't change size

--- js/equalityact.js
+++ js/equalityact.js
@@ -1,18 +1,9 @@
 function changeTSize(nu) {
-  var el = document.getElementsByTagName('body')[0]; 
-     // only one body in document
+  var body = document.getElementsByTagName('body')[0]; 
+ 
   var fontSizeString = window
-     .getComputedStyle(el, null)
+     .getComputedStyle(body, null)
      .getPropertyValue('font-size');
   var fontSize = parseFloat(fontSizeString); 
-  el.style.fontSize = (fontSize + nu) + 'px';
-    // Must be done separately for buttons:
-  var els = document.getElementsByTagName('button');
-  for (var i = 0; i < els.length; i++) {
-     fontSizeString = window
-        .getComputedStyle(els[i], null)
-        .getPropertyValue('font-size');
-     var fontSize = parseFloat(fontSizeString); 
-     els[i].style.fontSize = (fontSize + nu) + 'px';
-  }
+  body.style.fontSize = (fontSize + nu) + 'px';
  }
