1e93f11608bd3b718091f3e7add71246bc4ee7db
Joeet33 <up931673@myport.ac.uk>
2020-03-29 21:20:54 +0100

    working

---
+++ js/rescale.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    function myFunction() {
+        var x = document.getElementById("topnav");
+        if (x.className === "nav") {
+          x.className += " responsive";
+        } else {
+          x.className = "nav";
+        }
+      }
+});
