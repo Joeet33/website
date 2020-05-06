e22b5af356e1ea18f340bb666099df13669ad0b1
Joeet33 <up931673@myport.ac.uk>
2020-05-06 15:20:09 +0100

    mobile application

--- js/index.js
+++ js/index.js
@@ -93,4 +93,114 @@ $("#main4").hide();
 $("#main5").show();
    
 });
+
+//mobile navigation start
+
+//When a click on the hamburger icon happens
+$("#hamburgermenu").click(function() {
+
+	
+  //test if the mymobilenav element is hidden
+    if($("#mymobilenav").is(":hidden")) {
+  
+      
+  //if the mymobilenav element is hidden then slide down the mymobilenav box to show contents
+    $("#mymobilenav").slideDown();
+  
+      
+  //and to reduce the height of the ,main element to 62vh (this allows the foorter to be in view).
+    $(".main").css({"height": "62vh"});
+  
+      
+  //else if the mymobilenav elements is visible
+    }else{
+  
+  //hide the mymobilenav element by sliding up
+    $("#mymobilenav").slideUp();
+  
+      
+  //and increase the .main element to 75vh so the footer stays at the bottom of the page
+    $(".main").css({"height": "75vh"});
+      
+    }
+    
+  
+    
+  });
+    
+  $("#mobilefindus").click(function() {
+    
+    //alert("page 2 button works");
+    
+      $("title").text("Tiny's Website Page 2");
+    
+      $("#main1").hide();
+    
+      $("#mymain2").show();
+    
+      $("#mymain3").hide();
+    
+      $("#mymain4").hide();
+    
+      $("#mymain5").hide();
+    
+  });
+    
+  $("#mobilevideogallery").click(function() {
+    
+    //alert("page 3 button works");
+    
+      $("title").text("Tiny's Website Page 3");	
+    
+      $("#main1").hide();
+    
+      $("#mymain2").hide();
+    
+      $("#mymain3").show();
+    
+      $("#mymain4").hide();
+    
+      $("#mymain5").hide();
+    
+      
+  });
+    
+  $("#mobilecontactus").click(function() {
+    
+    //alert("page 4 button works");
+    
+      $("title").text("Tiny's Website Page 4");	
+    
+      $("#main1").hide();
+    
+      $("#mymain2").hide();
+    
+      $("#mymain3").hide();
+    
+      $("#mymain4").show();
+    
+      $("#mymain5").hide();
+    
+    
+  });
+    
+  $("#mobilepicturegallery").click(function() {
+    
+    //alert("page 5 button works");
+    
+      $("title").text("Tiny's Website Page 5");	
+    
+      $("#main1").hide();
+    
+      $("#mymain2").hide();
+    
+      $("#mymain3").hide();
+    
+      $("#mymain4").hide();
+    
+      $("#mymain5").show();
+    
+    
+  });	
+
 });
