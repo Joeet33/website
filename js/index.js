99284af3391d23b37d48288aff5b38fa2ec62327
Joeet33 <up931673@myport.ac.uk>
2020-05-08 14:20:09 +0100

    cookiebar + modal gallery

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 // JavaScript Document
 
-$(document).ready(function(){
-
-alert("We use cookies on this site to enhance your user experience.\nFor a complete overview of all cookies used, please see your personal settings.");
-    
-$("#flex-item_1").click(function(){
-   
   //alert("Page 1 Button working");
  
 $("title").text("Home");
